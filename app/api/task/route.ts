5f195657b92a3b0b671c880f00636bcd3b275671
C0NS03L <58496201+C0NS03L@users.noreply.github.com>
2025-01-14 10:37:56 +0700

    refactor: move api pages to app router

---
+++ app/api/task/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type TaskAddRequest = {
+  title: string;
+  description: string;
+  completed: boolean;
+};
+
+export async function GET() {
+  try {
+    const todos = await prisma.task.findMany({
+      select: {
+        title: true,
+        completed: true,
+      },
+    });
+    return NextResponse.json(todos, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch todos" + error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: TaskAddRequest) {
+  try {
+    const { title, description, completed } = request;
+
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        completed,
+      },
+    });
+
+    // Convert BigInt fields to strings
+    const taskWithStringId = {
+      ...newTask,
+      id: newTask.id.toString(),
+    };
+
+    return NextResponse.json(taskWithStringId, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    NextResponse.json({ error: "Failed to add task" + error }, { status: 500 });
+  }
+}
